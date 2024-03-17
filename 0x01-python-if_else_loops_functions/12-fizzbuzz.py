e77b034dde2f975c29181da774dd7cb317078d9e
Akinola-Moses <akinolamosesmyself@gmail.com>
2024-03-17 23:47:49 +0100

    print numbers with fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def fizzbuzz():
     num = 0
-    for num in range(100):
+    for num in range(101):
         if ((num % 3) == 0):
             print("Fizz", end=' ')
         elif ((num % 5) == 0):
             print("Buzz", end=' ')
-        elif ((( num % 3) == 0) and (num % 5) == 0):
+        elif ((num % 15) == 0):
             print("FizzBuzz", end=' ')
         else:
             print("{}".format(num), end=' ')
