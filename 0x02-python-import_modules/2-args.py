e7ee1ece78963ad74d96aad7d3c3843535a072f2
Akinola-Moses <akinolamosesmyself@gmail.com>
2025-04-07 22:19:23 +0000

    Read Args and argv like C

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    args = sys.argv[1:]
+    args_count = len(args)
+
+    if args_count == 0:
+        print ("0 arguments.")
+
+    elif args_count == 1:
+         print("{} argument:".format(args_count))
+
+         for idx in range (args_count):
+             print("{}: {}".format(idx + 1, args[idx]))
+    else:
+        print("{} arguments:".format(args_count))
+
+        for idx in range (args_count):
+            print("{}: {}".format(idx + 1, args[idx]))
