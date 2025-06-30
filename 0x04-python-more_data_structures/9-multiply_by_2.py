0600372a3bb1812c7447408a641ccc8651238c4e
Akinola-Moses <akinolamosesmyself@gmail.com>
2025-06-30 10:19:36 +0000

    Multiply all values by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,5 +3,4 @@ def multiply_by_2(a_dictionary):
     copyof_adic = {}
     for key in a_dictionary:
         copyof_adic[key] = a_dictionary[key] * 2
-        mult_val = copyof_adic
-    return (mult_val)
+    return (copyof_adic)
