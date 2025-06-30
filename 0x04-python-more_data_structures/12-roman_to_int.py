05daa9bae533b594b194ee750643e084c18478ad
Akinola-Moses <akinolamosesmyself@gmail.com>
2025-06-30 16:17:03 +0000

    Convert Roman numeral to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,15 +2,13 @@
 def roman_to_int(roman_string):
     if (type(roman_string) != str or roman_string is None):
         return (0)
-    roman_num = {
-            'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000,
-            }
+    rom_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     roman_total = 0
     next_value = 0
 
     for charc in reversed(roman_string):
-        if charc in roman_num:
-            immediate_val = roman_num[charc]
+        if charc in rom_num:
+            immediate_val = rom_num[charc]
         else:
             immediate_val = 0
         if immediate_val < next_value:
